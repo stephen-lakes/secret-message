3d4cc41bbd64f32f02c35b9b9352f524d33b3977
stephen <oluyomiolamilekan99@gmail.com>
2021-04-05 22:36:50 +0100

    Add files via upload

---
+++ problem6.py
@@ -0,0 +1,13 @@
+from string import ascii_lowercase
+
+
+LETTERS = {letter: str(index) for index, letter in enumerate(ascii_lowercase, start=1)}
+
+def alphabet_position(text):
+    text = text.lower()
+
+    numbers = [LETTERS[character] for character in text if character in LETTERS]
+
+    return '-'.join(numbers)
+
+print(alphabet_position("abcdefghijds"))
