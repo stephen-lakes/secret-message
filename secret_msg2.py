fa1476f47e7e32618c9bc6d73551a9fbeea2e6b5
stephen <oluyomiolamilekan99@gmail.com>
2021-04-14 14:15:33 +0100

    Created secret_msg2
    Modified version of secret_msg

---
+++ secret_msg2.py
@@ -0,0 +1,13 @@
+aphabet = "abcdefghijklmnopqrtsuvwxyz"
+input_string = input("Your msg> ")
+input_string = input_string.lower()
+
+for letter in input_string:
+    if letter not in aphabet:
+        input_string = input_string.replace(letter, " " + "-")
+    else:
+        input_string = input_string.replace(letter, str(aphabet.find(letter) + 1) + "-")
+    
+input_string = input_string[: -1]
+    
+print(input_string)
